1cbb1638e9aa7d3bde6a4621651693ee0372fd87
sheng yeong <chia.shengyeong@gmail.com>
2022-09-21 10:23:54 +0800

    more monster code

--- 02_monster-killer-project/assets/scripts/app.js
+++ 02_monster-killer-project/assets/scripts/app.js
@@ -3,21 +3,52 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt("Maximum life for you and your mosnter", "100");
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  hasBonusLife = true;
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
+
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+    alert('You are saved by bonus life. 😮‍💨');
+  }
+
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You won! ⚔️');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('You lost! 😭');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('You have a draw. 😵‍💫');
+  }
+
+  if (
+    currentMonsterHealth <= 0 || currentPlayerHealth <= 0
+  ) {
+    reset();
   }
 }
 
