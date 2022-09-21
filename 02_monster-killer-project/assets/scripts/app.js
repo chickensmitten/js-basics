0893db2d9e20bc75c927db49f4c7b3b9ec538138
sheng yeong <chia.shengyeong@gmail.com>
2022-09-21 10:00:51 +0800

    refactor attack handler

--- 02_monster-killer-project/assets/scripts/app.js
+++ 02_monster-killer-project/assets/scripts/app.js
@@ -1,23 +1,39 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
-let currentMonsterHeath = chosenMaxLife;
-let currentPlayerHeath = chosenMaxLife;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHeath -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHeath -= playerDamage;
-  if (currentMonsterHeath <= 0 && currentPlayerHeath > 0) {
-    alert("You won! ⚔️");
-  } else if (currentPlayerHeath <= 0 && monsterHealthBar < 0) {
-    alert("You lost! 😭");
-  } else if (currentPlayerHeath <= 0 && monsterHealthBar <= 0) {
-    alert("You have a draw. 😵‍💫") 
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === "STRONG_ATTACK") {
+    maxDamage = STRONG_ATTACK_VALUE;
   }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won! ⚔️');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost! 😭');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw. 😵‍💫');
+  }  
+}
+
+function attackHandler() {
+  attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+  attackMonster("STRONG_ATTACK");
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
