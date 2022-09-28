19080f39435b075a0212b63fb592fe0276c54303
sheng yeong <chia.shengyeong@gmail.com>
2022-09-28 15:19:10 +0800

    form submission with prevent default

--- 11_events-project/assets/scripts/events.js
+++ 11_events-project/assets/scripts/events.js
@@ -1,13 +1,13 @@
-const buttons = document.querySelectorAll("button");
+// const buttons = document.querySelectorAll("button");
 
-const buttonClickHandler = event => {
-  event.target.disabled = true;
-  console.log(event);
-};
+// const buttonClickHandler = event => {
+//   event.target.disabled = true;
+//   console.log(event);
+// };
 
-const anotherButtonClickHandler = () => {
-  console.log("this was clicked");
-}
+// const anotherButtonClickHandler = () => {
+//   console.log("this was clicked");
+// }
 
 // // this is not recommended because you can't add more logic. better to use addEventListener
 // button.onclick = buttonClickHandler;
@@ -31,10 +31,44 @@ const anotherButtonClickHandler = () => {
 //   button.removeEventListener("click", buttonClickHandler);
 // }, 2000);
 
-buttons.forEach(btn => {
-  btn.addEventListener("mouseneter", buttonClickHandler);
+// listening to mouse over events
+// buttons.forEach(btn => {
+//   btn.addEventListener("mouseenter", buttonClickHandler);
+// })
+
+// // listening to scroll events
+// window.addEventListener("scroll", event => {
+//   console.log(event)
+// });
+
+// infinite scrolling
+let curElementNumber = 0;
+ 
+function scrollHandler() {
+    const distanceToBottom = document.body.getBoundingClientRect().bottom;
+ 
+    if (distanceToBottom < document.documentElement.clientHeight + 150) {
+        const newDataElement = document.createElement('div');
+        curElementNumber++;
+        newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
+        document.body.append(newDataElement);
+    }
+}
+ 
+window.addEventListener('scroll', scrollHandler);
+
+` 
+So what's happening here?
+At the very bottom, we register the scrollHandler function as a handler for the 'scroll' event on our window object.
+Inside that function, we first of all measure the total distance between our viewport (top left corner of what we currently see) and the end of the page (not just the end of our currently visible area) => Stored in distanceToBottom.
+For example, if our browser window has a height of 500px, then distanceToBottom could be 684px, assuming that we got some content we can scroll to.
+Next, we compare the distance to the bottom of our overall content (distanceToBottom) to the window height + a certain threshold (in this example 150px). document.documentElement.clientHeight is preferable to window.innerHeight because it respects potential scroll bars.
+If we have less than 150px to the end of our page content, we make it into the if-block (where we append new data).
+Inside of the if-statement, we then create a new <div> element and populate it with a <p> element which in turn outputs an incrementing counter value.
+`
+
+const form = document.querySelector("form");
+form.addEventListener("submit", event => {
+  event.preventDefault(); // prevent default behaviour of the browser. In this case, it prevents the page from reloading when clicking submit form
+  console.log(event);
 })
-
-
-
-
